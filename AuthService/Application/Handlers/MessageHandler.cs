d455cc06b01138d704b77b690fbf277b6a0b4ac1
Sergio M <sergiomorenomartinez544@gmail.com>
2024-03-19 21:01:32 +0100

    Fixed dockerfiles + RabbitMQMessages lib (#8)
    * Added Rabbit MQ
    
    * Fixed docker compose
    
    * Fixed dockerfiles
    
    * Update docker-compose.yml

---
+++ AuthService/Application/Handlers/MessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using AuthService.Application.Clients;
+using EasyNetQ;
+using RabbitMQMessages;
+
+
+namespace AuthService.Application.Handlers {
+    public class MessageHandler : BackgroundService {
+
+        private void HandleRequestAuthMessage(RequestAuthMsg msg) {
+            Console.WriteLine("Received request: " + msg.Username + " " + msg.Password);
+        }
+    
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+            Console.WriteLine("Message handler is running...");
+        
+            var messageClient = new MessageClient(
+                RabbitHutch.CreateBus("host=rabbitmq;port=5672;virtualHost=/;username=guest;password=guest"));
+        
+            messageClient.Listen<RequestAuthMsg>(HandleRequestAuthMessage, "authenticate");
+        
+            while(!stoppingToken.IsCancellationRequested) {
+                await Task.Delay(1000, stoppingToken);
+            }
+            Console.WriteLine("Message handler is stopping...");
+        }
+    }
+}
