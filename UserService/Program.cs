1c8f9ee93c25d373c56a362e3b05db172e13394e
Sergio M <sergiomorenomartinez544@gmail.com>
2024-04-02 18:27:20 +0200

    Fixes

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -2,7 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using UserService.Models;
 using UserService.Application.Clients;
 using UserService.Application.Handlers;
+using RabbitMQMessages;
 using EasyNetQ;
+using EasyNetQ.DI;
+using EasyNetQ.Serialization.NewtonsoftJson;
 using UserService.Core.Services;
 using UserService.Infrastructure.Repositories;
 
@@ -18,8 +21,8 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteDatabasePath")));
 
 // Add services
-builder.Services.AddSingleton(
-    new MessageClient(RabbitHutch.CreateBus("host=localhost:5672;port=5672;virtualHost=;username=guest;password=guest")));
+
+builder.Services.AddSingleton(new MessageClient(RabbitHutch.CreateBus("host=rabbitmq;port=5672;virtualHost=/;username=guest;password=guest")));
 builder.Services.AddHostedService<MessageHandler>();
 builder.Services.AddScoped<UserManager>();
 builder.Services.AddScoped<UserRepository>();
@@ -29,6 +32,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-//app.MapControllers();
+app.MapControllers();
 
 app.Run();
