a73d96e5ab92b935d4e01df6fcd41b1cb9b7600c
Alex van der Steen <s1163665@student.windesheim.nl>
2024-04-03 11:52:39 +0200

    fix: FollowerService builds correctly now

--- FollowersService/Program.cs
+++ FollowersService/Program.cs
@@ -1,29 +1,39 @@
-﻿using FollowersService.Application.Clients;
+﻿using EasyNetQ;
+using Microsoft.EntityFrameworkCore;
+using FollowersService.Application.Clients;
 using FollowersService.Application.Handlers;
+using FollowersService.Models;
 using FollowersService.Core.Services;
-using EasyNetQ;
+using FollowersService.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add database context
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteDatabasePath")));
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton(new MessageClient(RabbitHutch.CreateBus("host=rabbitmq;port=5672;virtualHost=/;username=guest;password=guest")));
 builder.Services.AddHostedService<MessageHandler>();
 builder.Services.AddScoped<FollowingManager>();
+builder.Services.AddScoped<FollowersRepository>();
 
 var app = builder.Build();
 
-app.MapControllers();
+// Ensure the database is created and up-to-date
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
 
 app.Run();
