d32318ab7ed12141aa4b200355639836986455d6
Sergio M <sergiomorenomartinez544@gmail.com>
2024-04-03 10:56:56 +0200

    www

---
+++ RabbitMQMessages/User/UserMsg.cs
@@ -0,0 +1,7 @@
+﻿namespace RabbitMQMessages.User {
+    public class UserMsg {
+        public int Id { get; set; }
+        public string Username { get; set; } = default!;
+        public required bool Success { get; set; }
+    }
+}
