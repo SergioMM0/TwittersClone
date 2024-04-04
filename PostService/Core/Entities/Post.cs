3109230d4496ce3f84de3a69a59b295d4fc3065c
Sergio M <sergiomorenomartinez544@gmail.com>
2024-04-04 16:50:56 +0200

    Update Post.cs

--- PostService/Core/Entities/Post.cs
+++ PostService/Core/Entities/Post.cs
@@ -1,13 +1,28 @@
 ﻿namespace PostService.Core.Entities;
 
 public class Post {
+    /// <summary>
+    /// Id of the post
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// Title of the post
+    /// </summary>
     public required string Title { get; set; }
 
+    /// <summary>
+    /// Body of the post
+    /// </summary>
     public required string Body { get; set; }
 
+    /// <summary>
+    /// Id of the author of the post
+    /// </summary>
     public required int AuthorId { get; set; }
 
-    public List<int>? CommentsId { get; set; }
+    /// <summary>
+    /// Represents the parent post of this post
+    /// </summary>
+    public int ParentId { get; set; }
 }
